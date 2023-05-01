5fc0df2d68660dfd3fa0c58d36fb0c261e0f3b63
bekhzad <bekzodhamidullayev77@gmail.com>
2023-05-01 04:40:32 -0700

    some changes

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from .models import Post
 
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text='just a test')
 # Create your tests here.
